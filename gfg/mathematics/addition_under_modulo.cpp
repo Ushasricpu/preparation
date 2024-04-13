25c921270b4cb62db6e5f60eb4c6783168a046da
ushasrimogadali123 <ushasrimogadali123@gmail.com>
2024-04-13 23:44:52 +0530

    addition_in_modulo

---
+++ gfg/mathematics/addition_under_modulo.cpp
@@ -0,0 +1,25 @@
+//geometric progression
+
+#include<iostream>
+#include<math.h>
+#include <boost/multiprecision/cpp_int.hpp>
+using namespace std;
+using namespace boost::multiprecision;
+
+
+ long long addition_under_modulo(long long a, long long b){
+  
+    long long k=pow(10,9)+7;
+    long long c=a%k;
+    long long d=b%k;
+    return (c+d)%k;
+}
+
+int main(){
+    long long a,b;
+    cout<<"enter a"<<endl;
+    cin>>a;
+    cout<<"enter b"<<endl;
+    cin>>b;
+    cout<<"sum of numbers is"<< addition_under_modulo(a,b)<<endl;
+}

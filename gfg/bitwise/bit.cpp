e1df29309cdd4bfd8ba060e0abd42d43f9c11e6e
ushasrimogadali123 <ushasrimogadali123@gmail.com>
2024-04-17 00:47:34 +0530

    bit wise condition modified

--- gfg/bitwise/bit.cpp
+++ gfg/bitwise/bit.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 
 bool bit(int n,int k ){
-    if(n&k>=1){
+    if((n>>k)&1){
         return true;
     }
     return false;
@@ -19,3 +19,13 @@ int main(){
     cin>>k;
     cout<<bit(n,k)<<endl;
     }
+
+//if return as true and fals echeck function name as bool
+// n=2, k=3 
+//0010 right shift to 3 positions =  0 0 1 0(here 0 1 0 will be cancelled) = 0
+//0 & 1 = 0 
+
+
+//n=5, k=1
+//0101 right shift to one position = 0 1 0 1(here 1 will be cancelled) = 1
+//1 & 1 =1
